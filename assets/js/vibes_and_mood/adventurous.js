5e847bb7fef218e3bf0eb54bc941661e48991d5b
Mizuki Wakabayashi <wakamizuki.sub@gmail.com>
2025-04-25 09:52:56 +0900

    add: two type vibes&mood

---
+++ assets/js/vibes_and_mood/adventurous.js
@@ -0,0 +1,104 @@
+const activities = [
+    
+    {
+        title: "Forest Adventure",
+        description: "Forest Adventure is a network of outdoor adventure parks across Japan, inspired by a concept that originated in France.",
+        image: "/assets/images/Forest_Adventure_fixed.png",
+        link: "/src/activities/Forest_Adventure.html"
+    },
+    {
+        title: "Kerama Islands",
+        description: "The Kerama Islands are one of the best destinations in Okinawa for snorkeling, thanks to their crystal-clear waters.",
+        image: "/assets/images/KeramaIslands_1.jpeg",
+        link: "/src/activities/Snorkeling_in_Kerama.html"
+    },
+    {
+        title: "Zoorasia",
+        description: "Zoorasia is a large zoo in Yokohama known for its naturalistic animal exhibits and rare species like the okapi and Sumatran tiger.",
+        image: "/assets/images/Zoorasia_0.png",
+        link: "/src/activities/Zoorasia.html"
+    },
+    {
+        title: "Mount Fuji (Mt. Fuji)",
+        description: "Climbing Mt. Fuji is a popular activity among international tourists, and it typically takes two days and one night.",
+        image: "/assets/images/Mount_Fuji_1.png",
+        link: "/src/activities/Mount_Fuji.html"
+    },
+    {
+        title: "Oshino Shinobi no Sato",
+        description: "Oshino Shinobi no Sato is a NINJA-themed park located at the base of Mt. Fuji, especially popular with families and visitors interested in ninja culture.",
+        image: "/assets/images/Oshino_Ninja_1.png",
+        link: "/src/activities/Oshino_Ninja.html"
+    },
+    {
+        title: "Belluna Dome",
+        description: "Belluna Dome, located in Saitama, is the unique home of the Saitama Seibu Lions, known for its retractable roof and natural surroundings.",
+        image: "/assets/images/Belluna_Dome_1.jpg",
+        link: "/src/activities/Belluna_Dome.html"
+    },
+    {
+        title: "Suzuhiro Kamaboko Museum",
+        description: "Suzuhiro Kamaboko Museum is a Japanese food museum where visitors can learn about and experience making traditional fish cakes.",
+        image: "/assets/images/Suzuhiro_Kamaboko_Museum_1.png",
+        link: "/src/activities/Suzuhiro_Kamaboko_Museum.html"
+    },
+    {
+        title: "Fuji-Q Highland",
+        description: "Fuji-Q Highland is renowned for its thrilling attractions set against the stunning backdrop of Mount Fuji.",
+        image: "/assets/images/Fujikyu_fixed.png",
+        link: "/src/activities/Fujikyu.html"
+    },
+    {
+        title: "Fujiyama Adventure Club",
+        description: "Join us at Fujiyama Adventure Club for an exhilarating rafting experience on the beautiful and dynamic rivers of Japan.",
+        image: "/assets/images/FujiyamaAdventurePark_1.jpg",
+        link: "/src/activities/Fujiyama_Adventure_Club.html"
+    },
+    {
+        title: "Es Con Field Hokkaido",
+        description: "Es Con Field Hokkaido is a modern baseball stadium and the home field for the Hokkaido Nippon-Ham Fighters.",
+        image: "/assets/images/Es_Con_Field1.png",
+        link: "/src/activities/Es_Con_Field.html"
+    },
+    {
+        title: "Mazda Zoom-Zoom Stadium Hiroshima",
+        description: "Mazda Zoom-Zoom Stadium is the home field for the Hiroshima Toyo Carp, known for its fan-friendly design and proximity to the field.",
+        image: "/assets/images/Mazda_ZoomZoom_Stadium_1.jpg",
+        link: "/src/activities/Mazda_ZoomZoom_Stadium.html"
+    },
+    {
+        title: "Sagami Lake MORI MORI",
+        description: "Sagami Lake MORI MORI offers a variety of outdoor activities surrounded by beautiful nature.",
+        image: "/assets/images/MORI_MORI_fixed.png",
+        link: "/src/activities/MORI_MORI.html"
+    },
+    {
+        title: "Rakuten Mobile Park Miyagi",
+        description: "Rakuten Mobile Park Miyagi is the home stadium of the Tohoku Rakuten Golden Eagles, offering a fantastic experience for fans with modern amenities and a great atmosphere.",
+        image: "/assets/images/rakuten_mobile_park_miyagi_1.jpg",
+        link: "/src/activities/rakuten_mobile_park_miyagi.html"
+    },
+    {
+        title: "Sarugakyo Bungy Jump",
+        description: "Experience the thrill of bungee jumping in the beautiful Sarugakyo area surrounded by nature.",
+        image: "/assets/images/Sarugakyo_Bungy_1.jpg",
+        link: "/src/activities/Sarugakyo_Bungy.html"
+    },
+    {
+        title: "Takachiho Gorge: A Natural Wonder",
+        description: "Takachiho Gorge is a breathtaking canyon in Japan known for its stunning waterfalls and lush greenery, perfect for nature lovers and hikers.",
+        image: "/assets/images/Takachiho_Gorge_1.jpg",
+        link: "/src/activities/Takachiho_Gorge.html"
+    },
+    {
+        title: "Tottori Sand Dunes",
+        description: "Experience Japan's largest sand dunes located in Tottori prefecture, offering stunning views and outdoor activities.",
+        image: "/assets/images/Tottori_Sand_Dunes_1.jpg",
+        link: "/src/activities/Tottori_Sand_Dunes.html"
+    },
+
+];
+
+module.exports = activities;
+
+
