f899d8c461a4042401bf95a084115dae558184bf
Mizuki Wakabayashi <wakamizuki.sub@gmail.com>
2025-04-27 16:39:29 +0900

    add: nostalgic

---
+++ assets/js/vibes_and_mood/nostalgic.js
@@ -0,0 +1,73 @@
+const activities = [
+    {
+        title: "Hakone Shrine: A Serene Oasis",
+        description: "Hakone Shrine is a peaceful Shinto shrine located by Lake Ashi, surrounded by lush greenery and offering stunning views of Mount Fuji.",
+        image: "/assets/images/Hakone_Shrine_1.jpg",
+        link: "/src/activities/Hakone_Shrine.html"
+    },
+    {
+        title: "Gora Park: A Nature Lover's Paradise",
+        description: "Gora Park is a beautiful botanical garden located in the heart of the city, offering serene greenery and stunning floral displays.",
+        image: "/assets/images/Gora_Park_1.jpg",
+        link: "/src/activities/Gora_Park.html"
+    },
+    {
+        title: "Historic Old Hakone Road",
+        description: "Experience the beauty and history of Japan as you walk along the ancient Old Hakone Road, lined with traditional tea houses and stunning views of Mount Fuji.",
+        image: "/assets/images/Old_Hakone_Road_1.jpg",
+        link: "/src/activities/Old_Hakone_Road.html"
+    },
+    {
+        title: "Odawara Castle",
+        description: "Odawara Castle offers a glimpse into Japan’s samurai past with its well-preserved architecture and scenic views.",
+        image: "/assets/images/Odawara_Castle_1.jpg",
+        link: "/src/activities/Odawara_Castle.html"
+    },
+    {
+        title: "Matsumoto Castle",
+        description: "Matsumoto Castle, also known as the 'Crow Castle' due to its black exterior, is one of Japan’s most beautiful and historic castles. Surrounded by mountains and a serene moat, it offers a glimpse into Japan's feudal past.",
+        image: "/assets/images/Matsumoto_Castle_1.jpg",
+        link: "/src/activities/Matsumoto_Castle.html"
+    },
+    {
+        title: "Narai Juku: A Traditional Edo Period Post Town",
+        description: "Experience the charm of a well-preserved Edo period post town with traditional wooden buildings and a nostalgic atmosphere.",
+        image: "/assets/images/Narai_Juku_1.jpg",
+        link: "/src/activities/Narai_Juku.html"
+    },
+    {
+        title: "Hitachi Seaside Park",
+        description: "Hitachi Seaside Park is a vast national park located in Hitachinaka City, Ibaraki Prefecture.",
+        image: "/assets/images/Hitachi_fixed.png",
+        link: "/src/activities/Hitachi_Seaside_Park.html"
+    },
+
+    {
+        title: "Himeji Castle",
+        description: "Known as the 'White Heron Castle,' Himeji Castle is a UNESCO World Heritage Site and Japan's most famous historic castle.",
+        image: "/assets/images/Himeji_Castle_1.jpg",
+        link: "/src/activities/Himeji_Castle.html"
+    },
+    {
+        title: "Hikone Castle",
+        description: "A designated National Treasure, Hikone Castle is one of the few original castles in Japan and offers scenic views of Lake Biwa.",
+        image: "/assets/images/Hikone_Castle_1.jpg",
+        link: "/src/activities/Hikone_Castle.html"
+    },
+    {
+        title: "Inokashira Park",
+        description: "Inokashira Park is a beautiful public park in Tokyo known for its scenic pond, cherry blossoms, and proximity to the Ghibli Museum.",
+        image: "/assets/images/Inokashira_Park_1.png",
+        link: "/src/activities/Inokashira_Park.html"
+    },
+    {
+        title: "Shirakawa Village - A UNESCO World Heritage Site",
+        description: "Experience the charm of traditional Gassho-style houses in this picturesque village nestled in the Japanese Alps.",
+        image: "/assets/images/Shirakawa_Village_1.jpg",
+        link: "/src/activities/Shirakawa_Village.html"
+    },
+
+
+
+];
+module.exports = activities;
