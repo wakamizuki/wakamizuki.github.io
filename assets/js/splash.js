99e2f3252beb64ca77887b9c368e59b061bfca8d
Mizuki Wakabayashi <wakamizuki.sub@gmail.com>
2025-05-03 23:40:24 +0900

    add sakura

--- assets/js/splash.js
+++ assets/js/splash.js
@@ -1,6 +1,23 @@
 // splash.js
 
 window.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM fully loaded"); // ← これを追加
+    const sakuraContainer = document.querySelector('.sakura-container');
+    if (sakuraContainer) {
+        
+        for (let i = 0; i < 100; i++) {
+            const sakura = document.createElement('div');
+            if (Math.random() < 0.5) {
+                sakura.classList.add('dark'); // 50%で濃い色を適用
+            }
+            sakura.classList.add('sakura');
+            sakura.style.left = Math.random() * 100 + 'vw';
+            sakura.style.top = -35 + Math.random() * 30 + 'vh';  // -10vh 〜 20vh の範囲
+            sakura.style.animationDelay = Math.random() * 0.4 + 's';  // 0〜0.5秒の間に開始
+            sakura.style.animationDuration = 1.0 + Math.random() * 2.2 + 's';  // 0.8〜1.5秒で落下完了
+            sakuraContainer.appendChild(sakura);
+        }
+    }
     setTimeout(() => {
         document.getElementById('splash').style.display = 'none';
         document.getElementById('gradient-wrapper').style.display = 'block';
