98382145ad900d13754480a36fb79a4503e159f9
Mizuki Wakabayashi <wakamizuki.sub@gmail.com>
2025-05-07 17:57:55 +0900

    add

---
+++ assets/js/activity/Matsumoto_Castle.js
@@ -0,0 +1,8 @@
+window.activities = [
+    {
+        title: "Narai Juku: A Traditional Edo Period Post Town",
+        description: "Experience the charm of a well-preserved Edo period post town with traditional wooden buildings and a nostalgic atmosphere.",
+        image: "/assets/images/Narai_Juku_1.jpg",
+        link: "/src/activities/Narai_Juku.html"
+    },
+];
