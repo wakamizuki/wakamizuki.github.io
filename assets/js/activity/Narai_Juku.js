db64c9430478215118fa5d5101c3a8c5ca548ae1
Mizuki Wakabayashi <wakamizuki.sub@gmail.com>
2025-05-07 16:55:29 +0900

    add

---
+++ assets/js/activity/Narai_Juku.js
@@ -0,0 +1,15 @@
+window.activities = [
+    {
+        title: "Matsumoto Castle",
+        description: "Matsumoto Castle, also known as the 'Crow Castle' due to its black exterior, is one of Japan’s most beautiful and historic castles. Surrounded by mountains and a serene moat, it offers a glimpse into Japan's feudal past.",
+        image: "/assets/images/Matsumoto_Castle_1.jpg",
+        link: "/src/activities/Matsumoto_Castle.html"
+    },
+
+];
+
+
+
+
+
+
---
+++ assets/js/display_activities_for_activity.js
@@ -0,0 +1,27 @@
+// splash.js
+window.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector("#local-activities-and-sightseeing .row");
+    // 安全に activities が存在しているかチェックも可
+    if (typeof activities !== 'undefined' && Array.isArray(activities)) {
+        activities.forEach(activity => {
+            const cardElement = document.createElement("div");
+            cardElement.classList.add("col-lg-6", "col-md-12", "col-sm-4", "col-6", "p-0", "pl-2", "pb-1");
+
+            cardElement.innerHTML = `
+                <div class="card custom-card custom-card-small">
+                    <a href="${activity.link}">
+                        <img src="${activity.image}" class="card-img-top" alt="${activity.title}">
+                    </a>
+                    <div class="card-body p-0">
+                        <h4 class="card-title">${activity.title}</h4>
+                        <p class="card-text">${activity.description}</p>
+                    </div>
+                </div>
+            `;
+            container.appendChild(cardElement);
+        });
+    }
+
+});
+/*
+*/
