5aa8525371acca22802739683b025e0380154e8e
Mizuki Wakabayashi <wakamizuki.sub@gmail.com>
2025-05-08 17:18:35 +0900

    add 3

---
+++ assets/js/activity/Japanese_Tea_Experience_Sayama.js
@@ -0,0 +1,20 @@
+window.activities = [
+    {
+        title: "Belluna Dome",
+        description: "Belluna Dome, located in Saitama, is the unique home of the Saitama Seibu Lions, known for its retractable roof and natural surroundings.",
+        image: "/assets/images/Belluna_Dome_1.jpg",
+        link: "/src/activities/Belluna_Dome.html"
+    },
+    {
+        title: "Seibu-en Amusement Park",
+        description: "Experience thrilling rides and attractions at Seibu-en Amusement Park, perfect for a fun day out with family and friends.",
+        image: "/assets/images/Seibu-en_Amusement_Park_1.jpg",
+        link: "/src/activities/Seibu-en_Amusement_Park.html"
+    },
+    {
+        title: "Tokorozawa Aviation Memorial Park",
+        description: "Explore the rich history of aviation in Japan at this unique park featuring vintage aircraft displays and interactive exhibits.",
+        image: "/assets/images/Tokorozawa_Aviation_Memorial_Park_1.jpg",
+        link: "/src/activities/Tokorozawa_Aviation_Memorial_Park.html"
+    },
+];
