703c85b74f36d071055bf3a61d7fbb567412c531
Mizuki Wakabayashi <wakamizuki.sub@gmail.com>
2025-05-09 10:11:49 +0900

    add

---
+++ assets/js/activity/Hakone_Mototsumiya_Shrine.js
@@ -0,0 +1,26 @@
+window.activities = [
+    {
+        title: "Hakone Glass Forest Museum",
+        description: "Experience the beauty of glass art in a serene forest setting at Hakone Glass Forest Museum.",
+        image: "/assets/images/Hakone_Glass_Forest_Museum_1.jpg",
+        link: "/src/activities/Hakone_Glass_Forest_Museum.html"
+    },
+    {
+        title: "Ashinoko Lake",
+        description: "Enjoy stunning views of Mount Fuji while cruising on this beautiful lake located in Hakone, Japan.",
+        image: "/assets/images/Ashinoko_Lake_1.jpg",
+        link: "/src/activities/Ashinoko_Lake.html"
+    },
+    {
+        title: "Hakone Shrine: A Serene Oasis",
+        description: "Hakone Shrine is a peaceful Shinto shrine located by Lake Ashi, surrounded by lush greenery and offering stunning views of Mount Fuji.",
+        image: "/assets/images/Hakone_Shrine_1.jpg",
+        link: "/src/activities/Hakone_Shrine.html"
+    },
+];
+
+
+
+
+
+
