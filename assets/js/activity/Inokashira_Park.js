0581daad194407c7cff0b587f0f0559e2acfcf5e
Mizuki Wakabayashi <wakamizuki.sub@gmail.com>
2025-05-09 11:20:32 +0900

    push

---
+++ assets/js/activity/Inokashira_Park.js
@@ -0,0 +1,8 @@
+window.activities = [
+    {
+        title: "Ghibli Museum",
+        description: "The Ghibli Museum is a museum themed around the works of Studio Ghibli, offering a unique experience of the film's world.",
+        image: "/assets/images/Ghibli_Museum_2.png",
+        link: "/src/activities/Ghibli_Museum.html"
+    },
+];
