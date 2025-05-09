4c2eb8c5945a55fc803694bc7e5f7c61ed47da75
Mizuki Wakabayashi <wakamizuki.sub@gmail.com>
2025-05-09 10:43:52 +0900

    add

---
+++ assets/js/activity/Takachiho_Gorge.js
@@ -0,0 +1,44 @@
+window.activities = [
+    {
+        title: "Forest Adventure",
+        description: "Forest Adventure is a network of outdoor adventure parks across Japan, inspired by a concept that originated in France.",
+        image: "/assets/images/Forest_Adventure_fixed.png",
+        link: "/src/activities/Forest_Adventure.html"
+    },
+    {
+        title: "Kerama Islands",
+        description: "The Kerama Islands are one of the best destinations in Okinawa for snorkeling, thanks to their crystal-clear waters.",
+        image: "/assets/images/KeramaIslands_1.jpeg",
+        link: "/src/activities/Snorkeling_in_Kerama.html"
+    },
+    {
+        title: "Zoorasia",
+        description: "Zoorasia is a large zoo in Yokohama known for its naturalistic animal exhibits and rare species like the okapi and Sumatran tiger.",
+        image: "/assets/images/Zoorasia_0.png",
+        link: "/src/activities/Zoorasia.html"
+    },
+    {
+        title: "Mount Fuji (Mt. Fuji)",
+        description: "Climbing Mt. Fuji is a popular activity among international tourists, and it typically takes two days and one night.",
+        image: "/assets/images/Mount_Fuji_1.png",
+        link: "/src/activities/Mount_Fuji.html"
+    },
+    {
+        title: "Oshino Shinobi no Sato",
+        description: "Oshino Shinobi no Sato is a NINJA-themed park located at the base of Mt. Fuji, especially popular with families and visitors interested in ninja culture.",
+        image: "/assets/images/Oshino_Ninja_1.png",
+        link: "/src/activities/Oshino_Ninja.html"
+    },
+    {
+        title: "Belluna Dome",
+        description: "Belluna Dome, located in Saitama, is the unique home of the Saitama Seibu Lions, known for its retractable roof and natural surroundings.",
+        image: "/assets/images/Belluna_Dome_1.jpg",
+        link: "/src/activities/Belluna_Dome.html"
+    },
+    {
+        title: "Suzuhiro Kamaboko Museum",
+        description: "Suzuhiro Kamaboko Museum is a Japanese food museum where visitors can learn about and experience making traditional fish cakes.",
+        image: "/assets/images/Suzuhiro_Kamaboko_Museum_1.png",
+        link: "/src/activities/Suzuhiro_Kamaboko_Museum.html"
+    },
+];
