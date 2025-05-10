46adbb9bdc682532a8ad21eb7df8048966239f95
Mizuki Wakabayashi <wakamizuki.sub@gmail.com>
2025-05-10 11:22:38 +0900

    add

---
+++ assets/js/activity/Suzuhiro_Kamaboko_Museum.js
@@ -0,0 +1,32 @@
+window.activities = [
+    {
+        title: "Odawara Castle",
+        description: "Odawara Castle offers a glimpse into Japan’s samurai past with its well-preserved architecture and scenic views.",
+        image: "/assets/images/Odawara_Castle_1.jpg",
+        link: "/src/activities/Odawara_Castle.html"
+    },
+    {
+        title: "Hakone Glass Forest Museum",
+        description: "Experience the beauty of glass art in a serene forest setting at Hakone Glass Forest Museum.",
+        image: "/assets/images/Hakone_Glass_Forest_Museum_1.jpg",
+        link: "/src/activities/Hakone_Glass_Forest_Museum.html"
+    },
+    {
+        title: "Ashinoko Lake",
+        description: "Enjoy stunning views of Mount Fuji while cruising on this beautiful lake located in Hakone, Japan.",
+        image: "/assets/images/Ashinoko_Lake_1.jpg",
+        link: "/src/activities/Ashinoko_Lake.html"
+    },
+    {
+        title: "Hakone Mototsumiya Shrine",
+        description: "Experience the serene beauty and historical significance of Hakone Mototsumiya Shrine, nestled in the lush greenery of Hakone, Japan.",
+        image: "/assets/images/Hakone_Mototsumiya_Shrine_1.jpg",
+        link: "/src/activities/Hakone_Mototsumiya_Shrine.html"
+    },
+];
+
+
+
+
+
+
