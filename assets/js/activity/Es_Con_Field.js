63812f9ae8c4a22ee6cea9f1345edd6b2a4e5dc8
Mizuki Wakabayashi <wakamizuki.sub@gmail.com>
2025-05-10 12:22:18 +0900

    add

---
+++ assets/js/activity/Es_Con_Field.js
@@ -0,0 +1,21 @@
+window.activities = [
+    {
+        title: "Belluna Dome",
+        description: "Belluna Dome, located in Saitama, is the unique home of the Saitama Seibu Lions, known for its retractable roof and natural surroundings.",
+        image: "/assets/images/Belluna_Dome_1.jpg",
+        link: "/src/activities/Belluna_Dome.html"
+    },
+    {
+        title: "Rakuten Mobile Park Miyagi",
+        description: "Rakuten Mobile Park Miyagi is the home stadium of the Tohoku Rakuten Golden Eagles, offering a fantastic experience for fans with modern amenities and a great atmosphere.",
+        image: "/assets/images/rakuten_mobile_park_miyagi_1.jpg",
+        link: "/src/activities/rakuten_mobile_park_miyagi.html"
+    },
+    {
+        title: "Mazda Zoom-Zoom Stadium Hiroshima",
+        description: "Mazda Zoom-Zoom Stadium is the home field for the Hiroshima Toyo Carp, known for its fan-friendly design and proximity to the field.",
+        image: "/assets/images/Mazda_ZoomZoom_Stadium_1.jpg",
+        link: "/src/activities/Mazda_ZoomZoom_Stadium.html"
+    },
+
+];
