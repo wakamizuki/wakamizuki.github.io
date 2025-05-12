ada81c1d541b6c038a45940127fcef1be443ede7
Mizuki Wakabayashi <wakamizuki.sub@gmail.com>
2025-05-12 15:30:51 +0900

    add mazda

---
+++ assets/js/activity/Mazda_ZoomZoom_Stadium.js
@@ -0,0 +1,20 @@
+window.activities = [
+    {
+        title: "Rakuten Mobile Park Miyagi",
+        description: "Rakuten Mobile Park Miyagi is the home stadium of the Tohoku Rakuten Golden Eagles, offering a fantastic experience for fans with modern amenities and a great atmosphere.",
+        image: "/assets/images/rakuten_mobile_park_miyagi_1.jpg",
+        link: "/src/activities/rakuten_mobile_park_miyagi.html"
+    },
+    {
+        title: "Belluna Dome",
+        description: "Belluna Dome, located in Saitama, is the unique home of the Saitama Seibu Lions, known for its retractable roof and natural surroundings.",
+        image: "/assets/images/Belluna_Dome_1.jpg",
+        link: "/src/activities/Belluna_Dome.html"
+    },
+    {
+        title: "Es Con Field Hokkaido",
+        description: "Es Con Field Hokkaido is a modern baseball stadium and the home field for the Hokkaido Nippon-Ham Fighters.",
+        image: "/assets/images/Es_Con_Field1.png",
+        link: "/src/activities/Es_Con_Field.html"
+    },
+];
