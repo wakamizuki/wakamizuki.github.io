b2a3925e156b3582192784da71c5d6764fae3837
Mizuki Wakabayashi <wakamizuki.sub@gmail.com>
2025-05-23 09:23:08 +0900

    fix

---
+++ assets/js/activity/Lake_Tazawa.js
@@ -0,0 +1,11 @@
+window.activities = [
+    {
+        title: "Oirase Gorge: A Scenic Nature Walk",
+        description: "Oirase Gorge is a picturesque river valley in Aomori Prefecture, famous for its crystal-clear stream, lush forests, and beautiful waterfalls along a well-maintained walking trail.",
+        image: "/assets/images/Oirase_Gorge_1.jpg",
+        link: "/src/activities/Oirase_Gorge.html",
+        region: "tohoku",
+        prefecture: "aomori"
+    }
+
+];
