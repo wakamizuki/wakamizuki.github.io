a6da02e7f77da15d9e03d38bc96b215d4bccd639
sutekami <gigamasi@gmail.com>
2025-06-17 23:28:22 +0900

    update: アクセシビリティ的にブロックのクリック動作による遷移はdivではなくaで行う #74

--- front/countryside/lib/components/shared/Header.tsx
+++ front/countryside/lib/components/shared/Header.tsx
@@ -4,10 +4,10 @@ export default function Header() {
   return (
     <div className="header w-full bg-red-500 h-20 p-4 flex items-center bg-linear-to-b from-[#df295c] to-[#fce8e3]">
       <div className="header__container w-full h-full flex justify-between">
-        <div className="header__logo flex items-center gap-2">
+        <a className="header__logo flex items-center gap-2" href="/">
           <Image className="w-10 h-10" alt="Japan Countryside Logo" src="/favicon.ico" width={20} height={20} />
           <div className="header__logo-title text-white text-4xl font-bold">Explore Japan's Countryside</div>
-        </div>
+        </a>
         <div className="header__lav flex-1">ここにナビゲーション</div>
       </div>
     </div>
