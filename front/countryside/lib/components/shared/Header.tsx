0406f5bd0da9ed83cb1459c04ad470e692c4c748
sutekami <gigamasi@gmail.com>
2025-06-18 22:42:41 +0900

    style: レスポンシブ細かく対応 #74

--- front/countryside/lib/components/shared/Header.tsx
+++ front/countryside/lib/components/shared/Header.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="header w-full bg-red-500 h-20 p-4 flex items-center bg-linear-to-b from-[#df295c] to-[#fce8e3]">
+    <div className="header w-full bg-red-500 h-20 md:pl-8 md:pr-8 p-4 flex items-center bg-linear-to-b from-[#df295c] to-[#fce8e3]">
       <div className="header__container w-full h-full flex justify-between">
         <a className="header__logo flex items-center gap-2" href="/">
           <Image className="w-10 h-10" alt="Japan Countryside Logo" src="/favicon.ico" width={40} height={40} />
           <div className="header__logo-title text-white lg:text-4xl text-2xl font-bold">
-            Explore Japan's<br className="lg:hidden"></br> Countryside
+            Explore Japan's<br className="md:hidden"></br> Countryside
           </div>
         </a>
         <div className="header__lav flex-1">ナビゲーション実装予定</div>
