a9e23d2ff6d56536bfe2566a6a641d26ecbaadb2
sutekami <gigamasi@gmail.com>
2025-06-18 22:47:13 +0900

    style: 画面上部固定にして影つけた #74

--- front/countryside/lib/components/shared/Header.tsx
+++ front/countryside/lib/components/shared/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="header w-full bg-red-500 h-20 md:pl-8 md:pr-8 p-4 flex items-center bg-linear-to-b from-[#df295c] to-[#fce8e3]">
+    <div className="header w-full bg-red-500 h-20 md:pl-8 md:pr-8 p-4 flex items-center bg-linear-to-b from-[#df295c] to-[#fce8e3] fixed shadow-sm">
       <div className="header__container w-full h-full flex justify-between">
         <a className="header__logo flex items-center gap-2" href="/">
           <Image className="w-10 h-10" alt="Japan Countryside Logo" src="/favicon.ico" width={40} height={40} />
