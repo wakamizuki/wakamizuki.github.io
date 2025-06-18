781096ab55a5bbedf0c53ce4f5f9b2bc226cf5d3
sutekami <gigamasi@gmail.com>
2025-06-18 22:48:14 +0900

    update: layoutのエントリポイント変更に伴いmetadataの記述位置の変更 #74

--- front/countryside/app/(root)/layout.tsx
+++ front/countryside/app/(root)/layout.tsx
@@ -1,11 +1,17 @@
 import Header from "@/lib/components/shared/Header";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: '[100% Countryside] Explore the Japanese Countryside – Villages, Nature & Rural Japan',
+  description: "Explore the beauty of the Japanese countryside – peaceful villages, lush mountains, and hidden hot springs across Japan. Your complete countryside Japan travel guide starts here.",
+  manifest: '/site.webmanifest',
+}
 
 export default function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // ここにnavを記述していく
   return (
     <div>
       <Header />
