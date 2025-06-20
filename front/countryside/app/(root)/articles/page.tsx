f2f716668f3848f05a5e663f24528cbf0940537b
sutekami <gigamasi@gmail.com>
2025-06-20 22:26:25 +0900

    add: markdown 作業途中 #74

--- front/countryside/app/(root)/articles/page.tsx
+++ front/countryside/app/(root)/articles/page.tsx
@@ -1,7 +1,15 @@
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeSanitize from 'rehype-sanitize';
+
 export default function ArticlesPage() {
   return (
     <div>
       <h1>this is articles page</h1>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeSanitize]}
+      />
     </div>
   );
 }
